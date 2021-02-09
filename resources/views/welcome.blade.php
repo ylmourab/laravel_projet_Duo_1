0c5ac86a20a061dcd832531dde5cb3af7a4c1e66
Dragfy11 <dragfy_dark@hotmail.com>
2021-02-09 09:26:14 +0100

    foreach correction

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,12 +7,13 @@
       <div class="row">
         <div class="col-lg-6 pt-2 pt-lg-0 order-2 order-lg-1 d-flex flex-column justify-content-center">
           <h1>{{$dataHomeElement[0]->titre1}}</h1>
+          <ul>
+          
           @foreach ($dataHomelist1 as $item)
-              <ul>
               <li><i class="ri-check-line"></i>{{$item->texte}}</li>
   
-              </ul>
-          @endforeach
+              @endforeach
+            </ul>
           
           <div class="mt-3">
             <a href="#about" class="btn-get-started scrollto">{{$dataHomeElement[0]->btn1}}</a>
@@ -20,7 +21,7 @@
           </div>
         </div>
         <div class="col-lg-6 order-1 order-lg-2 hero-img">
-          <img src="{{asset('img/hero-img.png')}}" class="img-fluid" alt="">
+          <img src="{{asset('img/'.$dataHomeElement[0]->img1)}}" class="img-fluid" alt="">
         </div>
       </div>
     </div>
@@ -38,17 +39,17 @@
             <h2>{{$dataHomeElement[0]->titre2}}</h2>
             <h3>{{$dataHomeElement[0]->subtitre}}</h3>
             {{-- rajouter une image --}}
-            <img class="img-fluid w-25" src="{{asset('img/orange.jpg')}}" alt="orange">
+            <img class="img-fluid w-25" src="{{asset('img/'.$dataHomeElement[0]->img2)}}" alt="orange">
           </div>
           <div class="col-lg-6 pt-4 pt-lg-0">
             <p>
               {{$dataHomeElement[0]->description1}}
             </p>
+            <ul>
             @foreach ($dataHomelist2 as $elem)
-                <ul>
                 <li><i class="ri-check-double-line"></i> {{$elem->texte}}</li>
-                </ul>
-            @endforeach
+                @endforeach
+              </ul>
             
             <p class="font-italic">
               {{$dataHomeElement[0]->description2}}
